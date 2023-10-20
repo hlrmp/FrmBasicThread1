d9a447bf91d63a1086db88b8afb9f4fdf87dc944
hlrmp <haysjhune@gmail.com>
2023-10-20 19:00:09 +0800

    Add project files.

---
+++ FrmBasicThread1/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace FrmBasicThread1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.RUN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "- Before starting the Thread -";
+            // 
+            // RUN
+            // 
+            this.RUN.BackColor = System.Drawing.Color.Red;
+            this.RUN.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RUN.Location = new System.Drawing.Point(141, 178);
+            this.RUN.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.RUN.Name = "RUN";
+            this.RUN.Size = new System.Drawing.Size(150, 42);
+            this.RUN.TabIndex = 1;
+            this.RUN.Text = "RUN";
+            this.RUN.UseVisualStyleBackColor = false;
+            this.RUN.Click += new System.EventHandler(this.RUN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(438, 313);
+            this.Controls.Add(this.RUN);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RUN;
+    }
+}
+
---
+++ FrmBasicThread1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RUN_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("- Before Starting Thread -");
+
+            ThreadStart delthread = new ThreadStart(MyThreadClass.Thread1);
+
+            Thread ThreadA = new Thread(delthread);
+            ThreadA.Name = "Thread A process = ";
+
+
+            Thread ThreadB = new Thread(delthread);
+            ThreadB.Name = "Thread A process = ";
+
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+
+
+            Console.WriteLine("- End of Thread -");
+
+            label1.Text = " - End of Thread - ";
+        }
+    }
+}
